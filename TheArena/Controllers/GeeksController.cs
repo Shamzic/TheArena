c0371bf6ecdff8c939f79dd4efed979a663c4bde
Jean-Baptiste <admin@jeanba88.com>
2018-12-03 15:41:01 -0500

    Modification des Geeks Meilleure gestion des authorisation d'accès, suppression du create car redondant avec le register. Suppression de la vue permettant une vue globale.

--- TheArena/Controllers/GeeksController.cs
+++ TheArena/Controllers/GeeksController.cs
@@ -20,46 +20,17 @@ namespace TheArena.Controllers
         // GET: Geeks
         public ActionResult Index()
         {
-            Geek loggedGeek = db.Geek.Where(g => g.Username == User.Identity.Name).FirstOrDefault();
-            if (loggedGeek.RolesGeek.Any(r => r.Roles.Name == "Admin" && r.Roles.Deleted != true))
-                return View(db.Geek.ToList());
-            else
-                return RedirectToAction("Details", new { id = loggedGeek.GeekId });
+         return RedirectToAction("Details", new { id = User.Identity.Name });
         }
 
         // GET: Geeks/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(string id)
         {
-            Geek geek = db.Geek.Find(id);
+            Geek geek = db.Geek.Where(p => p.Username == id).FirstOrDefault();
             if (geek == null)
             {
                 return HttpNotFound();
             }
-            return View(geek);
-        }
-
-        // GET: Geeks/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Geeks/Create
-        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
-        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GeekId,Username,Name,Surname,Password,Mail,Birthdate,Deleted")] Geek geek)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Geek.Add(geek);
-                db.SaveChanges();
-                
-
-                return RedirectToAction("Index");
-            }
-
             return View(geek);
         }
 
@@ -84,7 +55,7 @@ namespace TheArena.Controllers
         [HttpPost]
         [EditGeekAuthorized]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "GeekId,Username,Name,Surname,Password,Mail,Birthdate,Deleted")] Geek geek)
+        public ActionResult Edit([Bind(Include = "GeekId,Username,Name,Surname,Password,Mail,Birthdate")] Geek geek)
         {
             if (ModelState.IsValid)
             {
@@ -96,13 +67,13 @@ namespace TheArena.Controllers
         }
 
         // GET: Geeks/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Geek geek = db.Geek.Find(id);
+            Geek geek = db.Geek.Where(p => p.Username == id).FirstOrDefault();
             if (geek == null)
             {
                 return HttpNotFound();
@@ -113,9 +84,9 @@ namespace TheArena.Controllers
         // POST: Geeks/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(string id)
         {
-            Geek geek = db.Geek.Find(id);
+            Geek geek = db.Geek.Where(p => p.Username == id).FirstOrDefault();
             geek.Deleted = true;
             db.SaveChanges();
             return RedirectToAction("Index");
